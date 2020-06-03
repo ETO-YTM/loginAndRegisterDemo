a75c6e902915f7c2839132059fdebc7a7cdac244
ETO-YTM <60183283+ETO-YTM@users.noreply.github.com>
2020-06-03 18:20:38 +0800

    Update login.php

--- login.php
+++ login.php
@@ -13,11 +13,11 @@ session_start();
 <hr>
 
 <?php
-$connect = new mysqli("127.0.0.1", "root", "123456", "myfirstphpproj");
+$connect = new mysqli("127.0.0.1", "root", "123456", "myfirstphpproj");//此处连接自己的数据库
 $sqlSearch = "select username from userInfo where username= '$_POST[userName]' and password='$_POST[password]';";
 if (mysqli_num_rows($connect->query($sqlSearch))) {
     $_SESSION['user_id'] = $_POST['userName'];
-    setcookie('user_id', $_POST['userName'], time() + 60 * 60);
+    setcookie('user_id', $_POST['userName'], time() + 60 * 60);//设置cookie有效期为1小时
     echo '<script>window.location.href="HomePage.php"</script>';
 } else
     echo '<script>alert("用户名或密码错误！");window.location.href="index.php";</script>';
